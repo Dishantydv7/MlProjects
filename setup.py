0bd21360644ff0ed417175e67e71b03c5e2c2d9b
Dishant Yadav <dishantrao11@gmail.com>
2024-12-04 08:28:14 +0530

    Changed the setup.py to work now and handle the requirements

--- setup.py
+++ setup.py
@@ -1,23 +1,22 @@
-from setuptools import find_packages,setup
-from typing import List\
+from setuptools import find_packages, setup
+from typing import List
 
 removeVar = '-e .'
 
-def get_requirements(file_path:str)-> List[str]:
+def get_requirements(file_path: str) -> List[str]:
     req = []
     with open(file_path) as file_obj:
-        req = file_obj.readline()
-        req = [x.replace("\n"," ")for x in req ]
-
+        req = file_obj.readlines()  # Read all lines, not just the first
+        req = [x.strip() for x in req]  # Remove any leading/trailing whitespaces or newlines
         if removeVar in req:
-            req.remove(removeVar)
+            req.remove(removeVar)  # Remove the -e . line, as we don't need it here
     return req
 
 setup(
     name="MyMlModel",
-    version = "0.1",
-    author = "Dishant Yadav",
+    version="0.1",
+    author="Dishant Yadav",
     author_email="dishantrao11.code@gmail.com",
     packages=find_packages(),
-    install_requires = get_requirements('requirements.txt'),
+    install_requires=get_requirements('requirements.txt'),
 )
